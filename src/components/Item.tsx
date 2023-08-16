6d235069cdc700ed847e9f44e1c0ff1cff0d3577
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-08-16 02:01:37 +0100

    update item component

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-interface ItemProps {
-  text: string
+export interface ItemProps {
+  name: string,
+  value: number
 }
 
-export function Item({ text }: ItemProps) {
+export function Item({ name, value }: ItemProps) {
   return (
     <View style={styles.item}>
-      <Text style={styles.itemText}>{text}</Text>
+      <Text style={styles.itemValue}>{(value * 100).toFixed()}</Text>
+      <Text style={styles.itemText}>{name}</Text>
     </View>
   )
 }
@@ -15,7 +18,12 @@ export function Item({ text }: ItemProps) {
 const styles = StyleSheet.create({
   item: {
     width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
     paddingVertical: 16,
+    paddingHorizontal: 16,
+    marginVertical: 8,
     backgroundColor: '#f2f2f2',
     borderRadius: 4,
     shadowColor: '#000',
@@ -23,6 +31,20 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
   },
   itemText: {
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    fontSize: 16
+  },
+  itemValue: {
+    width: 48,
+    backgroundColor: '#50c8783b',
+    paddingHorizontal: 8,
+    paddingVertical: 10,
+    color: '#50c878',
+    borderWidth: 1,
+    borderColor: '#50c8785c',
+    fontWeight: '500',
+    borderRadius: 4,
     textAlign: 'center'
   }
 });
